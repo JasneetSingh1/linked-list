a30a259eb1b1577bbe48fadaa670652eef9b426a
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-03-04 14:55:56 -0700

    create getAt method to return node at specified index

--- linkedList.js
+++ linkedList.js
@@ -1,60 +1,66 @@
 import { Node } from "./node.js";
 
-class LinkedList{
-    
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 
-    constructor(){
-        this.head = null;
-        this.tail = null;
+  append(value) {
+    let newNode = new Node(value);
+
+    if (this.head == null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.nextNode = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  getTail() {
+    return this.tail;
+  }
+
+  size() {
+    let count = 0;
+    if (this.head == null) return 0;
+
+    let start = this.head;
+    while (start != null) {
+      start = start.nextNode;
+      count++;
     }
 
-    append(value){
-        let newNode = new Node(value);
+    return count;
+  }
 
-        if(this.head == null){
-            this.head = newNode;
-            this.tail = newNode;
-        }
-        else{
-            this.tail.nextNode = newNode;
-            this.tail = newNode;
-        }
+  prepend(value) {
+    let newNode = new Node(value);
+
+    if (this.head == null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.nextNode = this.head;
+      this.head = newNode;
+    }
+  }
+
+  getAt(index) {
+    let result = this.head;
+    for (let i = 0; i <= index; i++) {
+      if (result != null) {
+        result = result.nextNode;
+      }
     }
 
-    getHead(){
-        return this.head;
-    }
-
-    getTail(){
-        return this.tail;
-    }
-
-    size(){
-        let count = 0;
-        if(this.head == null) return 0;
-
-        let start = this.head;
-        while(start != null){
-            start = start.nextNode;
-            count++;
-        }
-
-        return count;
-    }
-
-    prepend(value){
-        let newNode = new Node(value);
-
-        if(this.head == null){
-            this.head = newNode;
-            this.tail = newNode;
-        }
-        else{
-            newNode.nextNode = this.head;
-            this.head = newNode;
-        }
-    }
+    return result;
+  }
 }
 
-
-export {LinkedList};
+export { LinkedList };
